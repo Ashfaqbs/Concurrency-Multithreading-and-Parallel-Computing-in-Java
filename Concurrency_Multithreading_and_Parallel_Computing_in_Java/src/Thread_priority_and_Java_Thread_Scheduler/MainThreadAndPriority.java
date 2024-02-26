5b915b51acce280a7434be4dba5b0d2f14bf6f73
Ashfaqbs <ash001x@gmail.com>
2024-02-26 19:47:09 +0530

    General Commit

--- Concurrency_Multithreading_and_Parallel_Computing_in_Java/src/Thread_priority_and_Java_Thread_Scheduler/MainThreadAndPriority.java
+++ Concurrency_Multithreading_and_Parallel_Computing_in_Java/src/Thread_priority_and_Java_Thread_Scheduler/MainThreadAndPriority.java
@@ -4,14 +4,12 @@ public class MainThreadAndPriority {
 
 	public static void main(String[] args) {
 
-		
-//		off course we can use the value within the range from 1-10 
+//		off course we can use the value within the range from 1-10 more then 10 we will run time exception
 //		Thread.currentThread().setPriority(Thread.MAX_PRIORITY); //Name of the Thread : main and its Priority is : 10
 
 //		Thread.currentThread().setPriority(Thread.MIN_PRIORITY); //Name of the Thread : main and its Priority is : 1
 
 //		Thread.currentThread().setPriority(Thread.NORM_PRIORITY);//Name of the Thread : main and its Priority is : 5
-
 
 		System.out.println("Name of the Thread : " + Thread.currentThread().getName() + " and its Priority is : "
 				+ Thread.currentThread().getPriority());
---
+++ Concurrency_Multithreading_and_Parallel_Computing_in_Java/src/Thread_priority_and_Java_Thread_Scheduler/ThreadPriority.java
@@ -0,0 +1,42 @@
+package Thread_priority_and_Java_Thread_Scheduler;
+
+import java.util.Iterator;
+
+class WorkerThread implements Runnable {
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Worker thread with max priority : " + i);
+		}
+	}
+}
+
+public class ThreadPriority {
+
+	public static void main(String[] args) {
+
+		
+////**		Higher priority threads are executed before lower priority threads but it depends
+//		on the underlying OS (thread starvation is avoided) which means if a given thread 
+//		higher priority is going to be executed 1st then it may happen that a given thread 
+//		with low priority is not going to be executed at all and off course JVM and thread 
+//		scheduler wants to avoid this situation so this is why despite the fact the thread 
+//		has a high priority the thread with
+//		lower priority will be executed as well by the CPU for a little time slice or time 
+//**//		
+		//thread with max priority 10
+		Thread t = new Thread(new WorkerThread());
+		t.setPriority(Thread.MAX_PRIORITY);
+		t.start();
+
+		//Main thread with default priority 5 
+		System.out.println("this is main thread ");
+
+		
+		
+		
+	}
+
+}
