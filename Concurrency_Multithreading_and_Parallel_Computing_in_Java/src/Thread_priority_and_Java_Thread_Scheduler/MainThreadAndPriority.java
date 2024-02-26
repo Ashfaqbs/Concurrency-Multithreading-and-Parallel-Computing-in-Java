73c05ac840dff7b517fc7872a3675e6aaabebfec
Ashfaqbs <ash001x@gmail.com>
2024-02-26 18:11:16 +0530

    General Commit

---
+++ Concurrency_Multithreading_and_Parallel_Computing_in_Java/src/Thread_priority_and_Java_Thread_Scheduler/MainThreadAndPriority.java
@@ -0,0 +1,20 @@
+package Thread_priority_and_Java_Thread_Scheduler;
+
+public class MainThreadAndPriority {
+
+	public static void main(String[] args) {
+
+		
+//		off course we can use the value within the range from 1-10 
+//		Thread.currentThread().setPriority(Thread.MAX_PRIORITY); //Name of the Thread : main and its Priority is : 10
+
+//		Thread.currentThread().setPriority(Thread.MIN_PRIORITY); //Name of the Thread : main and its Priority is : 1
+
+//		Thread.currentThread().setPriority(Thread.NORM_PRIORITY);//Name of the Thread : main and its Priority is : 5
+
+
+		System.out.println("Name of the Thread : " + Thread.currentThread().getName() + " and its Priority is : "
+				+ Thread.currentThread().getPriority());
+
+	}
+}
